fae09ffd00c1cc5937fea1a573f7898b4ed03744
Welson Teles <welsonlimawlsn@gmail.com>
2021-06-06 19:54:36 -0300

    Health check

--- src/main/java/br/com/escovandobit/amazon/AmazonApplication.java
+++ src/main/java/br/com/escovandobit/amazon/AmazonApplication.java
@@ -2,12 +2,22 @@ package br.com.escovandobit.amazon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/health-check")
 public class AmazonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmazonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonApplication.class, args);
+    }
+
+    @GetMapping
+    public String healthCheck() {
+        return "Applicação OK";
+    }
 
 }
